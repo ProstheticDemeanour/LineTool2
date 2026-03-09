bfc50a8c6b836b4d24a13a89d0d704b7f8d02b95
Kurejsepi <denan.kurejsepi@aecom.com>
2026-03-09 11:19:29 +0800

    Remove build dir + db from tracking, add .gitignore, add Author

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_LineTool_FILE C:/Users/kurejsepid/OneDrive/30 Engineering/31 Source/LineTool2/build/LineTool.exe)
-set(__QT_DEPLOY_TARGET_LineTool_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_LineTool_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
